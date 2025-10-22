cd7804cab60053bd877de70cb98328d2ea530a07
Renee Carignan <rcarigna@gmail.com>
2025-10-22 19:11:51 -0400

    Add ActionButtons tests and update @types/node version

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,24 +1,22 @@
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/**/*.test.ts',
-    '!src/**/*.spec.ts',
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
-    },
+export const testEnvironment = 'node';
+export const transform = {
+  '^.+\\.ts$': 'ts-jest',
+};
+export const testRegex = '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$';
+export const moduleFileExtensions = ['ts', 'js', 'json', 'node'];
+export const collectCoverageFrom = [
+  'src/**/*.ts',
+  '!src/**/*.d.ts',
+  '!src/**/*.test.ts',
+  '!src/**/*.spec.ts',
+];
+export const coverageDirectory = 'coverage';
+export const coverageReporters = ['text', 'lcov', 'html'];
+export const coverageThreshold = {
+  global: {
+    branches: 70,
+    functions: 70,
+    lines: 70,
+    statements: 70,
   },
 };
