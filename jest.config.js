aa355c0e71ca8959f8b3bdf94a95d2f4043323e7
Renee Carignan <rcarigna@gmail.com>
2025-10-23 09:30:21 -0400

    Refactor jest configuration to use module.exports syntax for server config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
-const path = require('path');
+const { join } = require('path');
 
 module.exports = {
   projects: [
     {
       displayName: 'client',
-      rootDir: path.join(__dirname, 'client'),
+      rootDir: join(__dirname, 'client'),
       ...require('./client/jest.config.cjs'),
+    },
+    {
+      displayName: 'server',
+      rootDir: join(__dirname, 'server'),
+      ...require('./server/jest.config.js'),
     },
   ],
 };
